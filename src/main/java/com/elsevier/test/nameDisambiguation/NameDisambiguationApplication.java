3b66e051c1b8ca43faa2a5fbde561b67c09dafc4
Konstantin <konstantn.petrov.ivanov@everis.com>
2021-10-23 13:27:39 +0200

    Initial code commit

---
+++ src/main/java/com/elsevier/test/nameDisambiguation/NameDisambiguationApplication.java
@@ -0,0 +1,13 @@
+package com.elsevier.test.nameDisambiguation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameDisambiguationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NameDisambiguationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elsevier/test/nameDisambiguation/NameDisambiguationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elsevier.test.nameDisambiguation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NameDisambiguationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
